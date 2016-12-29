3bb36dd1777ef6057a39c60611cdbbf9f9fc7cce
shenghao Yang <shenghao.yang@gmail.com>
2016-12-29 15:23:36 +0800

    update

--- _data/update_github.sh
+++ _data/update_github.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 jekyll build
-git commit -a -m "update" &&
-git checkout master &&
-rsync -av _site/ . &&
-git commit -a -m "update" &&
-git push &&
-git checkout develop
+git commit -a -m "update" && git checkout master && rsync -av _site/ . && git commit -a -m "update" && git push && git checkout develop
